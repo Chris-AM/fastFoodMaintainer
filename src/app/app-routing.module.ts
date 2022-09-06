e505756ee9ce05a66b84fda5043486cdea7b08eb
Chris-am <c.aranguizm@outlook.com>
2022-09-06 00:35:47 -0400

    :art: splitting modules by components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+
+// RoutingModules
+import { AuthRoutingModule } from './auth/auth.routing';
 import { PagesRoutingModule } from './pages/pages.routing';
 
+
 const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+  // path: dashboard: PagesRoutingModule,
+  // path: auths:     AuthRoutingModule
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
+    AuthRoutingModule,
     PagesRoutingModule
   ],
   exports: [RouterModule],
