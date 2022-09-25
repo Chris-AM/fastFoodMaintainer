1c65e22b05b0b3c7f15c20df74394d065639534c
Chris-am <c.aranguizm@outlook.com>
2022-09-24 23:23:09 -0300

    :sparkle: increasing bar feature implemented

--- src/app/components/increaser/increaser.component.ts
+++ src/app/components/increaser/increaser.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'app-increaser',
@@ -8,18 +14,37 @@ export class IncreaserComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`;
   }
-  public progress: number = 50;
 
-  get getProgrss() {
-    return `${this.progress}%`
-  }
+  @Input('increaser') progress: number = 50;
+  @Output('increaser') increasedValue: EventEmitter<number> = new EventEmitter();
+
+  @Input() btnClass: string = 'btn-primary';
 
   updateProgress(value: number) {
-    if(this.progress >= 100 && value >= 0) this.progress = 100;
-    if(this.progress <= 0 && value < 0) this.progress = 0;
-    this.progress = this.progress + value;
+    if(this.progress >= 100 && value >= 0) {
+      this.increasedValue.emit(100);
+      return this.progress = 100;
+    };
+    if(this.progress <= 0 && value < 0) {
+      this.increasedValue.emit(0);
+      return this.progress = 0;
+    };
+    this.increasedValue.emit( this.progress );
+    this.progress = this.progress + value; 
   }
 
+  onChange( writenInput:number ){
+    if (writenInput >= 100) {
+      this.progress = 100
+    }
+    if (writenInput <= 0) {
+      this.progress = 0
+    } else {
+      this.progress = writenInput;
+    }
+    this.increasedValue.emit( this.progress );
+  }
 }
--- src/app/pages/daily-sales/daily-sales.component.ts
+++ src/app/pages/daily-sales/daily-sales.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-daily-sales',
   templateUrl: './daily-sales.component.html',
   styleUrls: ['./daily-sales.component.css']
 })
-export class DailySalesComponent implements OnInit {
+export class DailySalesComponent {
+  product1: number = 10;
+  product2: number = 15;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getProduct1() {
+    return `${ this.product1 }%`;
   }
 
-
-
+  get getProduct2() {
+    return `${ this.product2 }%`;
+  }
+  
 }
