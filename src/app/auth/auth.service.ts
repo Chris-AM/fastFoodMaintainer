84853debecc1869c3c9ffb0ccc12a438cef7e38f
Chris-am <c.aranguizm@outlook.com>
2022-10-06 16:50:29 -0300

    :sparkle: regular login in front ready

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.electron';
 import { registerInterface } from './register/register.interface';
+import { LoginInterface } from './login/login.interface';
 
 const base_url = environment.base_url;
 
@@ -14,5 +15,9 @@ export class AuthService {
 
   createUser( formData: registerInterface ) {
     return this.httpClient.post(`${base_url}/auth/maintainer/register`, formData);
+  }
+
+  login( formData: LoginInterface ) {
+    return this.httpClient.post(`${base_url}/auth/login`,  formData);
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +16,28 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public loginForm = new FormGroup({
+    email: new FormControl('c.aranguizm@outlook.com', [Validators.email]),
+    password: new FormControl('1704', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(16)
+    ]),
+    remember: new FormControl(false),
+  });
+
+  constructor(
+    private readonly router: Router,
+    private readonly authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   login(){
-    this.router.navigateByUrl('/');
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (data) => console.log(data),
+      error: (err) => Swal.fire('error', err.error.error, 'error'),
+    });
   }
 }
