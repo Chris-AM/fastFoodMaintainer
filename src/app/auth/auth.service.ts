74ab587fae92761bdc8282b707bc7845796bdd3c
Chris-am <c.aranguizm@outlook.com>
2022-10-06 17:15:54 -0300

    :sparkle: storing token in localstorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.electron';
 import { registerInterface } from './register/register.interface';
 import { LoginInterface } from './login/login.interface';
+import { tap } from 'rxjs';
 
 const base_url = environment.base_url;
 
@@ -11,13 +12,27 @@ const base_url = environment.base_url;
 })
 export class AuthService {
 
-  constructor( private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  createUser( formData: registerInterface ) {
-    return this.httpClient.post(`${base_url}/auth/maintainer/register`, formData);
+  createUser(formData: registerInterface) {
+    return this.httpClient
+      .post(`${base_url}/auth/maintainer/register`, formData)
+      .pipe(
+        tap({
+          next: (resp: any) => localStorage.setItem('token', resp.token),
+          error: (err) => console.log('tap error', err),
+        })
+      );
   }
 
-  login( formData: LoginInterface ) {
-    return this.httpClient.post(`${base_url}/auth/login`,  formData);
+  login(formData: LoginInterface) {
+    return this.httpClient
+      .post(`${base_url}/auth/login`, formData)
+      .pipe(
+        tap({
+          next: (resp: any) => localStorage.setItem('token', resp.token),
+          error: (err) => console.log('tap error', err),
+        })
+      );
   }
 }
