5050d32473ac9aa06acfc5e0097920f30fc71e11
Chris-am <c.aranguizm@outlook.com>
2022-11-16 23:51:46 -0300

    :bug: solving unauthorized error by creating a global token interceptor

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,26 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { TokenInterceptorService } from '../shared/shared-services/token-interceptor.service';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent,
-  ],
-  exports: [
-    LoginComponent,
-    RegisterComponent,
-  ],
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     RouterModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-  ]
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
