201fc00f16c1fd8b3fb8dd51b231182e7356eaed
Chris-am <c.aranguizm@outlook.com>
2022-11-26 12:41:01 -0300

    :rocket: deploying front end Beta 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/v1'
+  base_url: 'http://ec2-18-208-110-224.compute-1.amazonaws.com/v1',
+  //base_url: 'http://localhost:3000/v1'
 };
 
 /*
